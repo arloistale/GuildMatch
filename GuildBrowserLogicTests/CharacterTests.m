588b62cd8e8a258704181b2bb53da9b3456230ff
Jonathan Lu <jonathanlu@Jonathans-MacBook-Pro.local>
2016-01-03 11:13:46 -0800

    Proper character info tests now

--- GuildBrowserLogicTests/CharacterTests.m
+++ GuildBrowserLogicTests/CharacterTests.m
@@ -35,6 +35,9 @@
     XCTAssertNotNil(json, @"Invalid test data");
     
     _characterDetailJSON = json;
+    _testCharacter = [[Character alloc] initWithCharacterDetailData:_characterDetailJSON];
+    
+    XCTAssertNotNil(_testCharacter, "Couldn't parse Character from detail JSON");
 }
 
 - (void)tearDown {
@@ -45,14 +48,11 @@
 }
 
 - (void)testCreateCharacterFromDetailJson {
-    _testCharacter = [[Character alloc] initWithCharacterDetailData:_characterDetailJSON];
-    XCTAssertNotNil(_testCharacter, "Couldn't parse Character from detail JSON");
-    
-    XCTAssertNotNil([[Character alloc] initWithCharacterDetailData:nil],
-                    "Couldn't parse Character from detail JSON");
+    Character *failingCharacter = [[Character alloc] initWithCharacterDetailData:nil];
+    XCTAssertNotNil(failingCharacter, "Couldn't parse Character from detail JSON");
 }
 
-- (void)testCreateCharacterFromDetailJSONProps {
+- (void)testCharacterInfo {
     XCTAssertEqualObjects(_testCharacter.thumbnail, @"borean-tundra/171/40508075-avatar.jpg", @"thumbnail url is wrong");
     XCTAssertEqualObjects(_testCharacter.name, @"Hagrel", @"name is wrong");
     XCTAssertEqualObjects(_testCharacter.battleGroup, @"Emberstorm", @"battlegroup is wrong");
@@ -65,6 +65,28 @@
     XCTAssertEqualObjects(_testCharacter.gender, @"Male", @"gener is wrong");
     XCTAssertEqualObjects(_testCharacter.averageItemLevel, @379, @"avg item level is wrong");
     XCTAssertEqualObjects(_testCharacter.averageItemLevelEquipped, @355, @"avg item level is wrong");
+}
+
+- (void)testCharacterItems {
+    XCTAssertEqualObjects(_testCharacter.neckItem.name,@"Stoneheart Choker", @"name is wrong");
+    XCTAssertEqualObjects(_testCharacter.wristItem.name,@"Vicious Pyrium Bracers", @"name is wrong");
+    XCTAssertEqualObjects(_testCharacter.waistItem.name,@"Girdle of the Queen's Champion", @"name is wrong");
+    XCTAssertEqualObjects(_testCharacter.handsItem.name,@"Time Strand Gauntlets", @"name is wrong");
+    XCTAssertEqualObjects(_testCharacter.shoulderItem.name,@"Temporal Pauldrons", @"name is wrong");
+    XCTAssertEqualObjects(_testCharacter.chestItem.name,@"Ruthless Gladiator's Plate Chestpiece", @"name is wrong");
+    XCTAssertEqualObjects(_testCharacter.fingerItem1.name,@"Thrall's Gratitude", @"name is wrong");
+    XCTAssertEqualObjects(_testCharacter.fingerItem2.name,@"Breathstealer Band", @"name is wrong");
+    XCTAssertEqualObjects(_testCharacter.shirtItem.name,@"Black Swashbuckler's Shirt", @"name is wrong");
+    XCTAssertEqualObjects(_testCharacter.tabardItem.name,@"Tabard of the Wildhammer Clan", @"nname is wrong");
+    XCTAssertEqualObjects(_testCharacter.headItem.name,@"Vicious Pyrium Helm", @"neck name is wrong");
+    XCTAssertEqualObjects(_testCharacter.backItem.name,@"Cloak of the Royal Protector", @"neck name is wrong");
+    XCTAssertEqualObjects(_testCharacter.legsItem.name,@"Bloodhoof Legguards", @"neck name is wrong");
+    XCTAssertEqualObjects(_testCharacter.feetItem.name,@"Treads of the Past", @"neck name is wrong");
+    XCTAssertEqualObjects(_testCharacter.mainHandItem.name,@"Axe of the Tauren Chieftains", @"neck name is wrong");
+    XCTAssertEqualObjects(_testCharacter.offHandItem.name,nil, @"offhand should be nil");
+    XCTAssertEqualObjects(_testCharacter.trinketItem1.name,@"Rosary of Light", @"neck name is wrong");
+    XCTAssertEqualObjects(_testCharacter.trinketItem2.name,@"Bone-Link Fetish", @"neck name is wrong");
+    XCTAssertEqualObjects(_testCharacter.rangedItem.name,@"Ironfeather Longbow", @"neck name is wrong");
 }
 
 - (void)testPerformanceExample {
